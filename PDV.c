d3e802cdc60a080221a28511143158f8547d4fc9
lucaspereirasouza <lucaspereiradesouza2163@outlook.com>
2024-09-29 18:59:50 -0300

    added listProduct

--- PDV.c
+++ PDV.c
@@ -62,3 +62,90 @@ void adicionar(){
 	fclose(cadastro.arq);
 	system ("clear");
 }
+
+void listarProdutos(){
+	
+cadastro.arq = fopen("func.txt", "r");
+char listas[200];
+
+
+while(fgets(listas,200,cadastro.arq)!=NULL){ 
+	printf("%s",listas); //printf p imprimir a lista na tela
+}
+fclose(cadastro.arq);
+system("pause"); 
+system ("clear");
+}
+
+void buscar(){
+    // Counters
+	int counter_produto = 0;
+	int counter_categoria = 0;
+	//
+    int j2 = 0;
+
+	// Bools
+    int bol_produto;
+	int bol_categoria;
+
+
+    // teste
+	int teste;
+ 
+	//char listas[200];
+
+	cadastro.arq = fopen("func.txt", "r");
+	printf("-----------------------------------------------------------------------\n");
+	printf("|                       BUSCAR PRODUTO                                |\n");
+	printf("-----------------------------------------------------------------------\n\n\n ");
+	fflush(stdin);
+	printf("Por favor,informe o nome do produto que deseja buscar:\n");
+
+	fgets(cadastro.buscar_Produto, 100, stdin);    
+
+	cadastro.buscar[strlen(cadastro.buscar) -1] = 0;  
+
+	do{
+		fgets(cadastro.nomeDoProduto, 100, cadastro.arq);        
+		counter_produto++;        
+		if(strstr(cadastro.nomeDoProduto, cadastro.nomeDoProduto) != NULL){
+			//printf("Linha %d: %s", j, cadastro.buscar);
+			bol_produto = 1;
+			break; 
+			//strcpy(pegar_nome,cadastro.nome);           
+			//achou1 = 1;        
+		}	
+        		    
+	}while(!feof(cadastro.arq));
+	rewind(cadastro.arq);
+	printf("Por favor,informe a categoria do produto que deseja buscar:\n");
+	fgets(cadastro.Categoria, 100, stdin);    
+	cadastro.Categoria[strlen(cadastro.Categoria) -1] = 0;  
+	do{
+		fgets(cadastro.Categoria, 1000, cadastro.arq);        
+		counter_categoria++;        
+		if(strstr(cadastro.Categoria, cadastro.buscar_categoria) != NULL){
+			//printf("Linha %d: %s", j1, cadastro.buscar_profissao);
+			//nao1 = j1;
+            bol_categoria = 1; 
+			break; 
+			//strcpy(pegar_nome,cadastro.nome);           
+			//achou1 = 1;        
+		}				    
+	}while(!feof(cadastro.arq));
+	// rewind(cadastro.arq);
+	if(bol_produto == 1 && bol_categoria == 1){
+		printf("\nFoi encontrado o funcionário !");
+		printf("\nNome - %s", cadastro.buscar);
+		printf("\nCategoria - %s", cadastro.buscar_categoria);
+
+	}
+	
+    // counter_produto counter_categoria
+	if(counter_produto != 1 && counter_categoria != 1){
+		printf("\nNão foi encontrado o produto!\n");	
+	}
+	fclose(cadastro.arq);
+	system("pause"); 
+	system ("clear");
+}
